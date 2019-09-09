d091f124e7198b6a3e7d19f9db96a7233bb46e8d
Gianluca <luke@daffre.com>
2019-09-09 21:02:31 +0200

    First Example

---
+++ example1/src/components/ClassComponent.js
@@ -0,0 +1,19 @@
+import React, { Component } from "react";
+
+class ClassComponent extends Component {
+
+    state = {
+        name: "Gianluca"
+    }
+
+    render() {
+        return (
+            <div>
+                <h1> hi my name is: {this.state.name}</h1>
+                <h2> with property: {this.props.name}</h2>
+            </div>
+        );
+    }
+}
+
+export default ClassComponent;
