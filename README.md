a2ed6338b7dfcac079a9a489259905d0c21aa375
Gianluca Daffré <daffre@nyp.lan>
2019-09-10 11:53:13 +0200

    Improvements

--- README.md
+++ README.md
@@ -1,1 +1,15 @@
-# reactExamples
+My own React examples
+# Available Examples #
+
+## Example 1: ##
+Difference between Component Class and Functional Component
+
+## Example 2: ##
+
+Bind **this** to a Class Method
+
+## Example 3: ##
+
+Router Example
+
+
--- example3/src/App.js
+++ example3/src/App.js
@@ -2,14 +2,16 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import LandingPage from './pages/home';
 import AboutPage from './pages/about';
-
+import { BrowserRouter } from 'react-router-dom';
 
 function App() {
   return (
-    <Switch>
-      <Route exact path={"/"} component={LandingPage} />
-      <Route exact path={"/about"} component={AboutPage} />
-    </Switch>
+    <BrowserRouter>
+      <Switch>
+        <Route exact path={"/"} component={LandingPage} />
+        <Route exact path={"/about"} component={AboutPage} />
+      </Switch>
+    </BrowserRouter>
   );
 }
 
