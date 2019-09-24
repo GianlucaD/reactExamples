26b6a14269d19f5451245e064a5c8c91e0c53664
Gianluca <luke@daffre.com>
2019-09-24 16:31:20 +0200

    bigfix

--- example9/src/components/Users.js
+++ example9/src/components/Users.js
@@ -6,61 +6,31 @@ export default class Users extends Component {
         users: []
     };
 
-    constructor(props) {
-        
-        super(props);
-        this.getData = this.getData.bind(this);
-    }
-
-    getData() {
-        axios.get("https://randomuser.me/api/?results=50")
-            .then(response => {
-                this.setState({ users: response.data });
-            })
-            .catch(e => console.error(e));
-    }
     componentDidMount() {
-        // try to simulate latency
-        setTimeout(
-            this.getData
-            , 3000);
+            axios.get("https://randomuser.me/api/?results=50")
+                .then(response => {
+                    this.setState({ users: response.data.results });
+                }, )
+                .catch(e => console.error(e))
     }
     render() {
-        /*
-        const users = this.state.users.map((user, ind) => {
-            return <li key={ind}>
-                {user.name.first}
-            </li>;
- 
-        })
-        */
-       const posts = this.state.users.map(post => {
-           return <User />
-       })
         if (this.state.users.length === 0) {
             return (<div>Loading...</div>);
         }
         else {
+            console.log(this.state.users);
             return (
                 <div>
                     <ul>
-                        <User></User>
-
+                        {this.state.users.map((data, ind) => {
+                            return <li key={ind}>
+                                {data.name.first}
+                            </li>;
+                        }
                         )}
                     </ul>
                 </div>
-
             );
         }
-        /* withoud axios: 
-        fetch("https://randomuser.me/api/?results=50")
-            .then(response => response.json())
-            //                .then(users => this.setState({ users }))
-            .then(data => {
-                console.log(data.results);
-                this.setState({ users: data.results })
-            })
-            .catch(e => console.error(e));
-            */
     }
 }
