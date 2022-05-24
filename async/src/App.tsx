f87a9e08c470c5e518eb4059c89923ae0f7877b7
Gianluca Daffre <gianluca.daffre@noseryoung.ch>
2022-05-24 13:02:51 +0200

    cleanup for better understanding

--- async/src/App.tsx
+++ async/src/App.tsx
@@ -10,21 +10,27 @@ function App() {
     const res = await fetch("https://jsonplaceholder.typicode.com/posts/1");
     const json = await res.json();
     setPost(json);
+    console.log("post", post);
   };
 
-  useEffect(() => {
+  const getTodo = () => {
     fetch("https://jsonplaceholder.typicode.com/todos/1")
       .then((response) => response.json())
       .then((json) => {
         setTodo(json);
       });
+    console.log("todo", todo);
+  };
+
+  useEffect(() => {
+    getTodo();
     getPost();
   }, []);
 
   return (
     <div className="App">
-      <p>Todo: {JSON.stringify(todo)}</p>
-      <p>Post: {JSON.stringify(post)}</p>
+      <p>Todo Promise: {JSON.stringify(todo)}</p>
+      <p>Post async/await: {JSON.stringify(post)}</p>
     </div>
   );
 }
